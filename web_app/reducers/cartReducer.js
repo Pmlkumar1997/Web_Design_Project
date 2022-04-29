0ddf3cdbbc98efacf6f2a6054b4af13fe663012a
kvsrinivas <kvsrinivas91@gmail.com>
2022-04-29 10:49:41 -0400

    Updated reducers

--- web_app/reducers/cartReducer.js
+++ web_app/reducers/cartReducer.js
@@ -1,8 +1,9 @@
 //handle functionality of carts increase and decrease no of products in cart 
 //method accepts current state of cart and action item and updates state in immutable manner 
+// This method is a cart reducer
 export const cartReducer = (state = { cartProducts: [] }, action) => {    
   switch (action.type) {
-    case "ADD_TO_CART":
+    case "ADD_TO_CART": // add item to cart
       const alreadyExists = state.cartProducts.find(
         (product) => product._id === action.payload._id
       );
@@ -20,7 +21,7 @@ export const cartReducer = (state = { cartProducts: [] }, action) => {
         };
       }
 
-    case "DELETE_FROM_CART":
+    case "DELETE_FROM_CART": // delete items from cart
       return {
         ...state,
         cartProducts: state.cartProducts.filter(
